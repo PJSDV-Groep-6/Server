e0d036bfd7a3133032ba11a275cded759870f146
Hello_User <s.soekhlal@aperturect.com>
2021-12-17 16:28:35 +0100

    linkedlists vervangen. begin gemaakt aan afhandeling van client requests.

--- main.cpp
+++ main.cpp
@@ -4,16 +4,22 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <string>
+#include <string.h>
 #include <stdexcept>
-
-#include "LinkedList.cpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 #define PORT 8080
 
 bool STOP(char* buffer);
 void endBuffer(char* buffer, size_t length);
+void clearBuffer(char* buffer) {
+    for (int i = 0; i <= sizeof(buffer); i++) {
+        buffer[i] = 0;
+    }
+}
 
 int main(int argc, char const* argv[])
 {
@@ -24,9 +30,8 @@ int main(int argc, char const* argv[])
     char buffer[1024] = { 0 };
     char numberString[24] = {0};
     
-    LinkedList<int> messageList;
-    Node<int>* node;
-    int id = 0, message = 0;
+    int id = 0;
+    std::string message;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -57,7 +62,8 @@ int main(int argc, char const* argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-
+    bool licht = false;
+    std::fstream statefile;
     while (!STOP(buffer)) {
         if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0){
             perror("accept");
@@ -65,18 +71,48 @@ int main(int argc, char const* argv[])
         }
         valread = read(new_socket, buffer, 1024);
         endBuffer(buffer, valread);
-        sscanf(buffer, "%d %d", &id, &message);
-        if (id == 2 && message == 0) {
-            messageList.addNode(3, 2);
+        std::istringstream receive(buffer);
+        receive >> id >> message;
+        switch (id) {
+            case 1:
+                if (message == "Beweging" && !licht) {
+                    send(new_socket, "licht", 9, 0);
+                    licht = true;
+                }
+                else if (message == "Beweging" && licht) {
+                    send(new_socket, "thcil", 9, 0);
+                    licht = false;
+                }
+            case 2:
+                if (message == "0" || message == "2") {
+                    send(new_socket, "1", 2, 0);
+                }
+                if (message == "1" || message == "3") {
+                    send(new_socket, "0", 2, 0);
+                }
+            case 3:
+                if (message == "1") {
+                    send(new_socket, "licht", 9, 0);
+                }
+                else if (message == "0") {
+                    send(new_socket, "thcil", 9, 0);
+                }
         }
-        strcpy(buffer, "");
-        while((node = messageList.removeNode(id)) != NULL){
-            sprintf(numberString, "%d ", node->getData());
-            strcat(buffer, numberString);
-            delete node;
+        statefile.open("states.txt", std::ios::in);
+        if (statefile.is_open()) {
+            std::string line;
+            std::string state;
+            int value = 0;
+            while (getline(statefile, line)) {
+                std::istringstream stream(line);
+                stream >> state >> value;
+                //std::cout << state << " " << value << std::endl;
+                //if(state == "drukKnopDeur")
+            }
         }
-        send(new_socket, buffer, 1024, 0);
+        //send(new_socket, buffer, 1024, 0);
         printf("%s\n", buffer);
+        clearBuffer(buffer);
         shutdown(new_socket, SHUT_RDWR);
     }
     return 0;
