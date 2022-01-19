3e7ebc44702161f7b1817a4a83ce93ba7158badd
Hello-User <20123493@student.hhs.nl>
2022-01-19 13:53:47 +0100

    server read functies nu in een apparte method

--- main.cpp
+++ main.cpp
@@ -8,26 +8,21 @@
 #include "headers/deur.h"
 
 bool STOP(char* buffer);
-void endBuffer(char* buffer, size_t length);
-void clearBuffer(char* buffer, const size_t length);
 
 int main(int argc, char const* argv[]) {
     std::vector<meubel*> meubels;
     meubels.push_back(new schemerlamp(1, "schemerLamp", "../states.cpp"));
     meubels.push_back(new deur(2, "deur", "../states.cpp"));
     meubels.push_back(new bed(3, "bedLamp", "../states.cpp"));
-    const size_t bufferSize = 1024;
-    char buffer[1024] = {0};
-    int valread;
     int id = 0;
+    char buffer[1024] = {0};
     std::string message;
     socketService server(8080);
     while (!STOP(buffer)) {
         server.sockAccept();
-        valread = server.sockRead(buffer);
-        endBuffer(buffer, valread);
-        std::istringstream receive(buffer);
-        receive >> id >> message;
+        server.parseInput(buffer);
+        id = server.id;
+        message = server.message;
         for(meubel* Meubel : meubels){
             Meubel->input(id, message);
             Meubel->check();
@@ -40,27 +35,11 @@ int main(int argc, char const* argv[]) {
             }
         }
         else server.sockSend("ok");
-        //clearBuffer(buffer, bufferSize);
         server.sockClose();
     }
     return 0;
 }
 
-void endBuffer(char* buffer, size_t length) {
-    if (length >= 0) {
-        buffer[length] = '\r';
-    }
-    else {
-        buffer[0] = '\r';
-    }
-}
-
 bool STOP(char* buffer) {
     return strncmp(buffer, "STOP", 4) == 0;
-}
-
-void clearBuffer(char* buffer, const size_t length) {
-    for (int i = 0; i <= length; i++) {
-        buffer[i] = 0;
-    }
 }
