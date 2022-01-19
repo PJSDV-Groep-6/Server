dd744ce1b22f527c8812aca964646d5c8f0936da
Tom <tommie2002@github.com>
2022-01-20 00:35:46 +0100

    Crossdevice werkt deels, istates kan geen spaties in de state hebben en moet bij elke nieuwe run geleegd worden om SEGFAULTS en SEGABORTS te voorkomen en het is tot nu toe alleen nog op schemerlamp toegepast voor druk op het bed als proof of concept

--- schemerlamp.cpp
+++ schemerlamp.cpp
@@ -1,22 +1,21 @@
 #include "headers/schemerlamp.h"
 
-schemerlamp::schemerlamp(int id, string name, const string& path, string internal): meubel(id, name, path) , statefile(path), log("log.txt"), lamp(false), istates(internal) {
+schemerlamp::schemerlamp(int id, string name, const string& path, string internal): meubel(id, name, path, internal) , statefile(path), log("log.txt"), lamp(false), istates(internal) {
     if(istates.gotoLine(name + "bewegingssensor") == -1) {
         istates.appendLine(name + "bewegingssensor 0");
         state = name + "bewegingssensor";
     }
 }
-}
 
 bool schemerlamp::input(int id, string message){
     if (id == this->id){
-        if (message == "Beweging" && !mem){
+        if (message == "Beweging" || checkIState("bedLampdruksensor") == "0" && !mem){
             zetState(true);
             zetIState(state, true);
             return true;
         }
-        else if (message == "Beweging" && mem) {
-            zetState(state, false);
+        else if (message == "Beweging" || checkIState("bedLampdruksensor") == "1" && mem) {
+            zetState(false);
         }
     }
     return false;
